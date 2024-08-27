6140dadcc7923d96e44c5079e73f2bb6803f307d
pjeevasrigit <pjeevasri@gmail.com>
2024-08-27 13:11:51 +0530

    cucumber_bdd_base_framework

---
+++ cucumberbdd-master/cucumberbdd-master/src/test/java/awesomecucumber/runners/MyTestNGRunnerTest.java
@@ -0,0 +1,20 @@
+package awesomecucumber.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        plugin = {"html:target/cucumber/cucumber.html"},
+        tags = "@smoke",
+        glue = {"awesomecucumber"},
+        features = "src/test/resources/awesomecucumber"
+)
+public class MyTestNGRunnerTest extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
